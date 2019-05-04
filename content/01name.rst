b9117f598ff5fc6d57da616e6e4a0f2d73edf395
Andrey Komendantyan <komendantyan@gmail.com>
2019-05-04 03:39:01 +0300

    initial commit

---
+++ content/01name.rst
@@ -0,0 +1,15 @@
+Название
+========
+
+:date: 2019-05-04 02:55
+:modified: 2019-05-04 02:55
+:tags: thats, awesome
+:category: yeah
+:slug: my-super-post
+
+Тема
+----
+
+- 123
+- 456
+- 789
---
+++ pelicanconf.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Andrey'
+SITENAME = 'RedStarTitle'
+SITEURL = ''
+
+THEME = 'theme/'
+
+PLUGIN_PATHS = ['plugins/']
+PLUGINS = ['i18n_subsites']
+JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
+I18N_TEMPLATES_LANG = 'en'
+DEFAULT_LANG = 'ru'
+LOCALE = 'ru_RU'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Moscow'
+
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
